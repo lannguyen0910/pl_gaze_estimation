b42c1941f6aaf50c3cad3043c86f96a219573e8a
lannguyen <18120051@student.hcmus.edu.vn>
2022-05-13 02:18:51 +0700

    update

--- scripts/train_mpiigaze_resnet_preact_all.sh
+++ scripts/train_mpiigaze_resnet_preact_all.sh
@@ -8,14 +8,13 @@ for test_id in {0..14}; do
             configs/base.yaml \
             configs/model/mpiigaze/resnet_preact.yaml \
             configs/dataset/mpiigaze.yaml \
-            configs/optimizer/sgd.yaml \
+            configs/optimizer/adamw.yaml \
             configs/scheduler/multistep.yaml \
         --options \
-            SCHEDULER.EPOCHS 40 \
+            SCHEDULER.EPOCHS 50 \
             SCHEDULER.MULTISTEP.MILESTONES '[30, 35]' \
             TRAIN.BATCH_SIZE 32 \
             VAL.BATCH_SIZE 256 \
-            OPTIMIZER.LR 0.1 \
             LOG.WANDB.PROJECT pl_mpiigaze \
             EXPERIMENT.ROOT_DIR experiments/mpiigaze \
             EXPERIMENT.TEST_ID ${test_id} \
